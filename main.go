550345b663e2ee2caa7e1b60247c171645130d0d
Numpkens <numpkins1228@gmail.com>
2025-11-26 10:11:24 +0400

    add different handlers

--- main.go
+++ main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/healthz", handlerReadiness)
+
+	fsHandler := http.FileServer(http.Dir("."))
+
+	strippedHandler := http.StripPrefix("/app", fsHandler)
+
+	mux.Handle("/app/", strippedHandler)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Printf("Starting server on %s", server.Addr)
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
 	}
 }
