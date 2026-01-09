2de54f6e2506a4eb07e57607af4c769dd2091395
Numpkens <numpkins1228@gmail.com>
2026-01-09 15:51:17 +0400

    Complete endpoints and validate data

--- main.go
+++ main.go
@@ -6,16 +6,53 @@ import (
 	"net/http"
 	"sync/atomic"
 	"time"
+	"encoding/json"
+	"strings"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
+type chirpParams struct {
+	Body string `json:"body"`
+}
+
+type chirpResponseError struct {
+	Error string `json:"error"`
+}
+
+type chirpResponseValid struct {
+	CleanedBody string `json:"cleaned_body"`
+}
+
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	params := chirpParams{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
+		return
+	}
+
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength{
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	cleaned := getCleanedBody(params.Body)
+
+	respondWithJSON(w, http.StatusOK, chirpResponseValid{
+		CleanedBody: cleaned,
+	})
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -27,14 +64,18 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	hits := cfg.fileserverHits.Load()
-	responseBody := fmt.Sprintf("Hits: %d", hits)
+	htmlTemplate := `<html>
+	<body>
+  	  <h1>Welcome, Chirpy Admin</h1>
+    	<p>Chirpy has been visited %d times!</p>
+	</body></html>`
+	responseBody := fmt.Sprintf(htmlTemplate, hits)
 
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(responseBody))
 }
 
-// handlerReset resets the hit counter back to zero.
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
 
@@ -43,19 +84,51 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hits count reset to 0"))
 }
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w, code, chirpResponseError{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func getCleanedBody(body string) string {
+	badWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		loweredWord := strings.ToLower(word)
+		if _, ok := badWords[loweredWord]; ok {
+			words[i] = "****"
+		}
+	}
+	return strings.Join(words, " ")
+}
+
 func main() {
 	apiCfg := &apiConfig{
 		fileserverHits: atomic.Int32{},
 	}
 
 	mux := http.NewServeMux()
-
-
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-
-	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
-
-	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
 
 	fsHandler := http.FileServer(http.Dir("."))
 	strippedHandler := http.StripPrefix("/app", fsHandler)
